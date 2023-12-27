82d00f5fed5f4180fadfa0403cab5bb1f8439ec5
tobetop123 <tobetop123@gmail.com>
2023-12-27 15:41:11 +0900

    FCM 연길중(실패)

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,41 @@
-// server.js
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const admin = require('firebase-admin');
+const serviceAccount = require('./baechu-83ecd-firebase-adminsdk-z3pu7-93130182a2.json');
+const connection = require('./db');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 const app = express();
 const port = 3000;
-
-// Maria DB 
-const connection = require('./db');
 
 app.use(bodyParser.json());
 app.use(cors());
 
 const users = [];
 
-app.post('/signup', (req, res) => {
-  const { username, password } = req.body;
+async function sendPushNotification(deviceToken, title, body) {
+  const message = {
+    token: deviceToken,
+    notification: {
+      title: title,
+      body: body,
+    },
+  };
+
+  try {
+    const response = await admin.messaging().send(message);
+    console.log('푸시 알림을 성공적으로 보냈습니다:', response);
+  } catch (error) {
+    console.error('푸시 알림 보내기 실패:', error);
+  }
+}
+
+app.post('/signup', async (req, res) => {
+  const { username, password, deviceToken } = req.body;
 
   const existingUser = users.find(user => user.username === username);
   if (existingUser) {
@@ -25,14 +44,17 @@ app.post('/signup', (req, res) => {
 
   const newUser = { username, password };
   users.push(newUser);
-  // console.log("users test sdasda:",users);
 
   let query = 'INSERT INTO test (id, password) VALUES (?, ?)';
-  let result =[
-    username,
-    password
-  ]
+  let result = [username, password];
   connection.query(query, result);
+
+  try {
+    // 회원가입 성공 시 푸시 알림 보내기
+    await sendPushNotification(deviceToken, '회원가입 완료', '회원가입이 성공적으로 완료되었습니다.');
+  } catch (error) {
+    console.error('푸시 알림 보내기 실패:', error);
+  }
 
   return res.status(201).json({ success: true, message: '회원가입이 성공적으로 완료되었습니다.' });
 });
--- baechu/App.js
+++ baechu/App.js
@@ -1,15 +1,45 @@
-// App.js
-
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './src/screens/HomeScreen';
 import SignupScreen from './src/screens/SignupScreen';
 import LoginScreen from './src/screens/LoginScreen';
+import { firebase } from '@react-native-firebase/app';
+import messaging from '@react-native-firebase/messaging';
+
+const firebaseConfig = {
+  // Firebase 프로젝트 설정 정보 입력
+};
+
+// Firebase 초기화
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+  useEffect(() => {
+    // FCM 토큰 얻기
+    messaging().getToken().then(token => {
+      console.log('FCM 토큰:', token);
+    });
+
+    // 푸시 알림 수신 이벤트 핸들러 등록
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      console.log('푸시 알림 수신:', remoteMessage);
+      // 여기서 푸시 알림을 처리하는 코드 추가
+    });
+
+    // 앱이 백그라운드 또는 종료 상태에서 푸시 알림을 클릭했을 때 실행되는 이벤트 핸들러 등록
+    messaging().setBackgroundMessageHandler(async remoteMessage => {
+      console.log('백그라운드 상태에서 푸시 알림 클릭:', remoteMessage);
+      // 여기서 백그라운드 상태에서 푸시 알림을 처리하는 코드 추가
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
